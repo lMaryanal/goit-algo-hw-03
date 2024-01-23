a758ceb995bb54edc1af8e9db26d5e8141e2a9a3
lMaryanal <maryana.shcherbak@gmail.com>
2024-01-23 19:23:53 +0200

    first_task

--- first_task.py
+++ first_task.py
@@ -1,1 +1,15 @@
-print("Hello")
+from datetime import datetime as dtdt
+
+
+def get_days_from_today(date):
+    try:
+        date = dtdt.strptime(date, "%Y-%m-%d")
+        tdelta = date.toordinal() - dtdt.today().toordinal()
+        return(tdelta)
+    except Exception:
+        print("please enter the date in the format: YYYY-MM-DD")
+        
+
+
+print(get_days_from_today('2021-10-09'))
+
