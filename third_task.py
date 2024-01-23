5fa0da7489171bc042167aa1954e522442f524cc
lMaryanal <maryana.shcherbak@gmail.com>
2024-01-24 00:36:01 +0200

    third_task

---
+++ third_task.py
@@ -0,0 +1,24 @@
+import re
+
+
+def normalize_phone(phone_number):
+    pattern = r"[;,\-:\*\. \(\)]"
+    normalised_number = re.sub(pattern, "", phone_number)
+    if normalised_number.isdigit():
+        normalised_number = "+38"+ normalised_number if len(normalised_number) == 10 else "+"+ normalised_number
+    return(normalised_number)
+
+
+raw_numbers = [
+    "067 123 4567",
+    "(095) 234-5678",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
